8486bb4afab9476240dee6c9570744ed862eb373
RyUKEnza <kenza_raj@hotmail.fr>
2024-03-24 16:43:47 -0700

    add console

--- console.py
+++ console.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        return True
+    def do_EOF(self, arg):
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
