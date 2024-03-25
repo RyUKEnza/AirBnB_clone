00678fe7cd96c6c8eb0db081d18fdd61ff4c15ee
RyUKEnza <kenza_raj@hotmail.fr>
2024-03-24 19:22:47 -0700

    Corrected fileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import json
+import os
 from os import path
 from models.base_model import BaseModel
 
 class FileStorage:
-    __file_path = "file.json"
+    __file_path = "file_storage.json"
     __objects = {}
+
+    def __init__(self):
+        self.__file_path = "file_storage.json"
 
     def all(self):
         return self.__objects
@@ -30,3 +34,17 @@ class FileStorage:
                     obj_instance = globals()[class_name](**value)
                     self.__objects[key] = obj_instance
 
+    def remove_old_objects(self):
+        max_objects = 10
+        if len(self.__objects) > max_objects:
+            keys_to_remove = list(self.__objects.keys())[:-max_objects]
+            for key in keys_to_remove:
+                del self.__objects[key]
+
+    def delete(self, obj=None):
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+        else:
+            print("Object not found")
